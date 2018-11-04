73fa5e6d7c8b7ddf9eee4bd978638ecdaaf3c078
unknown <mheininger96@gmail.com>
2018-11-04 16:55:01 -0500

    final commit

--- app.js
+++ app.js
@@ -1,58 +1,68 @@
 const axios = require('axios');
 const request = require('request'); 
+const express = require('express');
+const app = express();
 
-let recyclingDay;
-let trashDay; 
-let tomorrow;
-function trashRequest(){
-  return new Promise(resolve => {
-    request('https://www.pgh.st/locate/1036/Murray%20Hill%20Ave/', function (error, response, body) {
-    let info = JSON.parse(body);
-    recyclingDay = info[0].next_recycling_date;
-    trashDay = info[0].next_pickup_date;
-    resolve();
-    });
-  })
- 
-}
-
-function tomorrowsDate (){
+app.get("/", function(res, req){
+    let recyclingDay;
+    let trashDay; 
+    let tomorrow;
+    function trashRequest(){
     return new Promise(resolve => {
-    let today = new Date();
-    let dd = today.getDate() + 1;
-    let mm = today.getMonth()+1;
-    let yyyy = today.getFullYear();
-    tomorrow = mm + '-' + dd + '-' + yyyy;
-    resolve();
-})
-}
-
-function groupMePost(){
-    if (tomorrow == trashDay && tomorrow == recyclingDay){
-        axios.post("https://api.groupme.com/v3/bots/post", {
-        "bot_id"  : "841d972456842608d4b31af7cb",
-        "text"    : "Tomorrow is trash and recycling day"
+        request('https://www.pgh.st/locate/1036/Murray%20Hill%20Ave/', function (error, response, body) {
+        let info = JSON.parse(body);
+        recyclingDay = info[0].next_recycling_date;
+        trashDay = info[0].next_pickup_date;
+        resolve();
         });
-    }else if (tomorrow == trashDay){
-        axios.post("https://api.groupme.com/v3/bots/post", {
-        "bot_id"  : "841d972456842608d4b31af7cb",
-        "text"    : "Tomorrow is trash day"
-        });
+    })
+    
     }
-   
-}
-function alert(){
-        if (tomorrow == trashDay){
-        groupMePost();
-        }
-}
 
-async function finalPost(){
-    await trashRequest();
-    await tomorrowsDate();
-    alert();
-}
-finalPost();
+    function tomorrowsDate (){
+        return new Promise(resolve => {
+        let today = new Date();
+        let dd = today.getDate() + 1;
+        let mm = today.getMonth()+1;
+        let yyyy = today.getFullYear();
+        tomorrow = mm + '-' + dd + '-' + yyyy;
+        resolve();
+    })
+    }
+
+    function groupMePost(){
+        if (tomorrow == trashDay && tomorrow == recyclingDay){
+            axios.post("https://api.groupme.com/v3/bots/post", {
+            "bot_id"  : "841d972456842608d4b31af7cb",
+            "text"    : "Tomorrow is trash and recycling day"
+            });
+        }else if (tomorrow == "11-5-2018"){
+            axios.post("https://api.groupme.com/v3/bots/post", {
+            "bot_id"  : "841d972456842608d4b31af7cb",
+            "text"    : "Tomorrow is trash day"
+            });
+        }
+    
+    }
+    function alert(){
+            if (tomorrow == "11-5-2018"){
+            groupMePost();
+            }
+    }
+
+    async function finalPost(){
+        await trashRequest();
+        await tomorrowsDate();
+        alert();
+    }
+    finalPost();
+
+
+});
+app.listen(3000, function(){
+    console.log('server running');
+});
+
 
 
 
