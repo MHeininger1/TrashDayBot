431531d0d88c0c8d0d46fbbced9c3b3c75640fc3
unknown <mheininger96@gmail.com>
2018-11-04 23:04:08 -0500

    heroku fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    
+    res.send("beep boop");
     let recyclingDay;
     let trashDay; 
     let tomorrow;
